56ffdcf82331d7804d20b954040214175654f7f1
jasonzhang929 <zhangsiyuan929@gmail.com>
2021-01-21 18:54:45 -0600

    mac push

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 import gym
 import d4rl # Import required to register environments
-import numpy as np
 
 # Create the environment
-env = gym.make('minigrid-fourrooms-v0')
+env = gym.make('maze2d-umaze-v1')
 
 # d4rl abides by the OpenAI gym interface
 env.reset()
-print(env.step(env.action_space.sample()))
+env.step(env.action_space.sample())
 
-# # Each task is associated with a dataset
-# # dataset contains observations, actions, rewards, terminals, and infos
-# dataset = env.get_dataset()
-# print(dataset['observations']) # An N x dim_observation Numpy array of observations
-#
-# # Alternatively, use d4rl.qlearning_dataset which
-# # also adds next_observations.
-# dataset = d4rl.qlearning_dataset(env)
-# print(np.shape(dataset['observations'][0]))
-# print(dataset['actions'][:10])
-# print(dataset.keys())
+# Each task is associated with a dataset
+# dataset contains observations, actions, rewards, terminals, and infos
+dataset = env.get_dataset()
+print(dataset['observations']) # An N x dim_observation Numpy array of observations
+
+# Alternatively, use d4rl.qlearning_dataset which
+# also adds next_observations.
+dataset = d4rl.qlearning_dataset(env)
